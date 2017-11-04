8b70d6af887d8ec271db1a163516f25d821154c4
Tobias Schifftner <ts@ambimax.de>
2017-11-04 14:19:39 +0100

    Fixed phplint, added colors

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 set -e
-set -x
- 
+#set -x
+
+# Colors
+RED='\033[1;31m'
+YELLOW='\033[1;33m'
+ORANGE='\033[0;33m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 function error_exit {
-    echo "$1" 1>&2
+    echo "${RED}$1${NC}" 1>&2
     exit 1
 }
 
@@ -13,7 +20,10 @@ function cleanup {
     rm -rf "${BUILDENV}"
   fi
 }
- 
+
+function step {
+    echo "${GREEN}$1${NC}"
+}
 trap cleanup EXIT
 
 # check if this is a travis environment
@@ -31,37 +41,27 @@ if [ -z $MAGETESTSTAND_URL ] ; then
 fi
 
 
-echo
-echo "Create build environment"
-echo "------------------------------------------------"
+step "Create build environment"
 BUILDENV=`mktemp -d /tmp/mageteststand.XXXXXXXX`
 export BUILDENV=${BUILDENV}
 
-echo "Cloning ${MAGETESTSTAND_URL} to ${BUILDENV}"
+step "Cloning ${MAGETESTSTAND_URL} to ${BUILDENV}"
 git clone "${MAGETESTSTAND_URL}" "${BUILDENV}" || error_exit "Cloning MageTestStand failed"
 
-echo
-echo "Copy module to build environment"
-echo "------------------------------------------------"
+step "Copy module to build environment"
 cp -rf "${WORKSPACE}" "${BUILDENV}/.modman/" || error_exit "Cannot copy module to build environment"
 
-echo
-echo "Run lint tests"
-echo "------------------------------------------------"
+step "Run lint tests"
 ${BUILDENV}/tools/xmllint.sh "${WORKSPACE}" || error_exit "XML lint test failed"
-${BUILDENV}/tools/phplinit.sh "${WORKSPACE}" || error_exit "PHP lint test failed"
+${BUILDENV}/tools/phplint.sh "${WORKSPACE}" || error_exit "PHP lint test failed"
 
-echo
-echo "Install composer dependencies"
-echo "------------------------------------------------"
+step "Install composer dependencies"
 if [ -f composer.json ]; then
     ${BUILDENV}/tools/composer.phar require aoepeople/composer-installers:* || error_exit "Unable to install composer installers"
     ${BUILDENV}/tools/composer.phar install --dev --no-interaction || error_exit "Composer install failed"
 fi
 
-echo
-echo "Copy module dependencies to build environment"
-echo "------------------------------------------------"
+step "Copy module dependencies to build environment"
 if [ -f "${WORKSPACE}/composer.lock" ] ; then
   cp -f ${WORKSPACE}/composer.lock "${BUILDENV}/" || error_exit "Cannot copy composer.lock to build environment"
 fi
@@ -74,14 +74,10 @@ if [ -d "${WORKSPACE}/.modman" ] ; then
   cp -rf ${WORKSPACE}/.modman/* "${BUILDENV}/.modman/" || error_exit "Cannot copy .modman folder to build environment"
 fi
 
-echo
-echo "Install Magento"
-echo "------------------------------------------------"
+step "Install Magento"
 ${BUILDENV}/install.sh || error_exit "Magento installation failed"
 
-echo
-echo "Run Unit Tests"
-echo "------------------------------------------------"
+step "Run Unit Tests"
 PHPUNIT=${BUILDENV}/bin/phpunit
 if [ ! -f $PHPUNIT ]; then
     PHPUNIT=${BUILDENV}/vendor/bin/phpunit
