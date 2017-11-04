1a5fb628c61cdd6f4192300e3fe3e6b67bd8ebb3
Tobias Schifftner <ts@ambimax.de>
2017-11-04 13:48:51 +0100

    Added lint tests, added error_exit

--- setup.sh
+++ setup.sh
@@ -2,6 +2,10 @@
 set -e
 set -x
  
+function error_exit {
+	echo "$1" 1>&2
+	exit 1
+}
 
 function cleanup {
   if [ -z $SKIP_CLEANUP ]; then
@@ -26,29 +30,54 @@ if [ -z $MAGETESTSTAND_URL ] ; then
     MAGETESTSTAND_URL="https://github.com/AOEpeople/MageTestStand.git"
 fi
 
+echo
+echo "Run lint tests"
+echo "------------------------------------------------"
+tools/xmllint.sh .modman/ || error_exit "XML lint test failed"
+tools/phplinit.sh .modman/ || error_exit "PHP lint test failed"
+
+echo
+echo "Install composer dependencies"
+echo "------------------------------------------------"
+if [ -f composer.json ]; then
+    tools/composer.phar require aoepeople/composer-installers:* || error_exit "Unable to install composer installers"
+    tools/composer.phar install --dev --no-interaction || error_exit "Composer install failed"
+fi
+
+echo
+echo "Create build environment"
+echo "------------------------------------------------"
 BUILDENV=`mktemp -d /tmp/mageteststand.XXXXXXXX`
 
 echo "Cloning ${MAGETESTSTAND_URL} to ${BUILDENV}"
-git clone "${MAGETESTSTAND_URL}" "${BUILDENV}"
-cp -rf "${WORKSPACE}" "${BUILDENV}/.modman/"
+git clone "${MAGETESTSTAND_URL}" "${BUILDENV}" || error_exit "Cloning MageTestStand failed"
 
-# if module came with own dependencies that were installed, use these:
+echo
+echo "Copy module and dependencies to build environment"
+echo "------------------------------------------------"
+cp -rf "${WORKSPACE}" "${BUILDENV}/.modman/" || error_exit "Cannot copy module to build environment"
+
 if [ -d "${WORKSPACE}/vendor" ] ; then
-  cp -f ${WORKSPACE}/composer.lock "${BUILDENV}/"
-  cp -rf ${WORKSPACE}/vendor "${BUILDENV}/"
+  cp -f ${WORKSPACE}/composer.lock "${BUILDENV}/" || error_exit "Cannot copy composer.lock to build environment"
+  cp -rf ${WORKSPACE}/vendor "${BUILDENV}/" || error_exit "Cannot copy vendor folder to build environment"
 fi
+
 if [ -d "${WORKSPACE}/.modman" ] ; then
-  cp -rf ${WORKSPACE}/.modman/* "${BUILDENV}/.modman/"
+  cp -rf ${WORKSPACE}/.modman/* "${BUILDENV}/.modman/" || error_exit "Cannot copy .modman folder to build environment"
 fi
 
-${BUILDENV}/install.sh
+echo
+echo "Install Magento"
+echo "------------------------------------------------"
+${BUILDENV}/install.sh || error_exit "Magento installation failed"
 
+echo
+echo "Run Unit Tests"
+echo "------------------------------------------------"
 PHPUNIT=${BUILDENV}/bin/phpunit
 if [ ! -f $PHPUNIT ]; then
     PHPUNIT=${BUILDENV}/vendor/bin/phpunit
 fi
 
-cd ${BUILDENV}/htdocs
+cd ${BUILDENV}/htdocs || error_exit "Cannot switch to magento root in build environment"
 ${PHPUNIT} --colors -d display_errors=1
-
-export BUILDENV=${BUILDENV}
