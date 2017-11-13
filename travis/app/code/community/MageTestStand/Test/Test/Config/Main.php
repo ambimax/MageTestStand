5ee24ff737f5e369dc117193c8201645addd756c
Tobias Schifftner <ts@ambimax.de>
2017-11-13 13:07:01 +0100

    Added travis

---
+++ travis/app/code/community/MageTestStand/Test/Test/Config/Main.php
@@ -0,0 +1,14 @@
+<?php
+
+class Ambimax_GoogleTagManager_Test_Config_Main extends EcomDev_PHPUnit_Test_Case_Config
+{
+    public function testModuleActive()
+    {
+        $this->assertModuleIsActive();
+    }
+
+    public function testModuleVersion()
+    {
+        $this->assertModuleVersion('1.0.0');
+    }
+}
